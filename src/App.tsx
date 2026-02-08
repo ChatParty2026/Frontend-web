4327c42ca855b4b1a37f396a02aa871a91bb4684
sy <ssyeo_n@naver.com>
2026-02-09 00:45:55 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-blue-600 text-white">
+      <h1 className="text-3xl font-bold">Tailwind v4 설치 완료! ✨</h1>
+    </div>
+  );
+}
+export default App;
